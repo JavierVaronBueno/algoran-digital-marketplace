ebb84d036947c31cc91db67fce389e6506ee4dad
javier_varon <javoxdaemon@gmail.com>
2024-11-29 11:31:30 -0500

    smart contract update, test functions added, pending the delete contract function is failing to run the test.

--- projects/digital_marketplace/tests/digitalmarketplace_test.py
+++ projects/digital_marketplace/tests/digitalmarketplace_test.py
@@ -1,24 +1,2 @@
-from collections.abc import Iterator
-
-import pytest
-from algopy_testing import AlgopyTestContext, algopy_testing_context
-
-from smart_contracts.digitalmarketplace.contract import Digitalmarketplace
-
-
-@pytest.fixture()
-def context() -> Iterator[AlgopyTestContext]:
-    with algopy_testing_context() as ctx:
-        yield ctx
-
-
-def test_hello(context: AlgopyTestContext) -> None:
-    # Arrange
-    dummy_input = context.any.string(length=10)
-    contract = Digitalmarketplace()
-
-    # Act
-    output = contract.hello(dummy_input)
-
-    # Assert
-    assert output == f"Hello, {dummy_input}"
+def test():
+    pass
